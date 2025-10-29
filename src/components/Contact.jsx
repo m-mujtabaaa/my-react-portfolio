a749442c803cd3b84df276c62ab933af708b2206
m-mujtabaaa <mmkww2007@gmail.com>
2025-10-29 09:17:05 -0700

    Initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ function Contact() {
           <h2 className='text-white font-black text-[48px] text-center mb-[5vh]'>Keep In Touch</h2>
           <p className='text-[#b8b8b8ff] text-[17px] text-center'>I'm currently specializing in <span className='text-[#4E9FE5] '>Front-end Development</span>.</p>
             <p className='text-[#b8b8b8ff] text-[17px] text-center mb-[7vh]'>Feel free to get in touch and talk more about your projects.</p>
-            <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out ml-4'>Resume</button>
+            <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out ml-11'>Resume</button>
             <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out ml-4'>Email</button>
-            <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out ml-4'>LinkedIn</button>
+            <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out ml-4'>WhatsApp</button>
         </div>
     </div>
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <div className='w-[90%] mx-auto'>
-        <p className='text-white text-center font-[8px]'>Design and Developed by Muhammd Mujtaba.</p>
-         <p className='text-white text-center font-[8px]'>Build with <span className='text-[#4E9FE5]'>React.js</span> & <span className='text-[#4E9FE5]'>Tailwind css</span></p>
+    <div className='w-[90%] h-[60px] mx-auto'>
+        <p className='text-white text-center text-[15px]'>Design and Developed by Muhammd Mujtaba.</p>
+        <p className='text-white text-center text-[15px]'>Build with <span className='text-[#4E9FE5]'>React.js</span> & <span className='text-[#4E9FE5]'>Tailwind css</span>. Hosted on <span className='text-[#4E9FE5]'>Vercel</span></p>
     </div>
     </>
   )

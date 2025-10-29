b61d406aeaa9293fa0cff56eeea7668336f810ea
m-mujtabaaa <mmkww2007@gmail.com>
2025-10-29 00:56:23 -0700

    project div

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,10 +5,14 @@ function Project() {
     <>
         <div className='w-[90%] mx-auto pt-[25vh] px-[20px]'  id='Project'>
           <h2 className='text-white font-bold text-[40px] '>Project</h2>
-          <div className='flex gap-5 py-[5vh]'>
-            <div className='h-[60vh] w-[25vw] rounded-xl bg-[#0f1b2cff] border-b-[2px] border-white grow'></div>
-            <div className='h-[60vh] w-[25vw] rounded-xl bg-[#0f1b2cff] border-b-[2px] border-white grow'></div>
-            <div className='h-[60vh] w-[25vw] rounded-xl bg-[#0f1b2cff] border-b-[2px] border-white grow'></div>
+          <p className='text-[#b8b8b8ff] w-[40vw] pt-3 font-medium'>Here's some of my projects that I have worked on.</p>
+          <div className='py-[5vh]'>
+            <div className='h-[60vh] w-[45vw] rounded-xl bg-[#061120ff] border-b-[2px] border-white'>
+              <div className='h-[32.5vh] bg-purple-200 rounded-t-lg'></div>
+              <h2 className='text-white font-semibold py-3 px-4 text-[22px]'>TechServe Website</h2>
+              <div className='bg-[#636363ff] h-[1px]'/>
+              <p className='text-[#b8b8b8ff] py-3 px-4 font-medium'>This is my first project, Build with html and css only.</p>
+            </div>
          </div>
         </div>
     </>

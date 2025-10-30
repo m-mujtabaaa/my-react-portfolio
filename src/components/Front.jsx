f84962d712008e2247f500a638c28c0e9170c3e9
m-mujtabaaa <mmkww2007@gmail.com>
2025-10-30 00:06:54 -0700

    Initial commit

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -12,7 +12,7 @@ function Front() {
         <div className='h-[200px] w-[2px] bg-linear-to-t from-[#020913ff] to-[#4E9FE5]'> </div>
       </div>
         <div>
-        <h5 className='text-[#4E9FE5] font-semibold text-[33px] h-[20px]'>Assalam O Alaikum, I'm-</h5>
+        <h5 className='text-[#4E9FE5] font-semibold text-[33px] h-[25px]'>Assalam O Alaikum, I'm-</h5>
         <h3 className='text-white text-[85px] mb-1 font-black '>Muhammad Mujtaba</h3>
         <ReactTyped strings={["Full Stack Web Developer","Full Stack Web Developer",]} typespeed={10} backspeed={10} loop className='text-white text-[33px] font-medium' /> 
         <p className='text-[#b8b8b8ff] text-[19px] font-medium w-[50vw] mt-7 mb-10'>I am an aspiring web developer with a strong foundation in HTML, CSS, Tailwind, JavaScript and React.js. Recently completed a Diploma in Information Technology (2024–2025).</p>

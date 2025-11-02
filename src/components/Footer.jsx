18435bfec5162df10b91b485f9c25c1734dd8788
m-mujtabaaa <mmkww2007@gmail.com>
2025-11-02 07:47:52 -0800

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <>
     <div className='w-[90%] h-[60px] mx-auto'>
-        <p className='text-white text-center text-[15px]'>Design and Developed by Muhammd Mujtaba.</p>
-        <p className='text-white text-center text-[15px]'>Build with <span className='text-[#4E9FE5]'>React.js</span> & <span className='text-[#4E9FE5]'>Tailwind css</span>. Hosted on <span className='text-[#4E9FE5]'>Vercel</span></p>
+        <p className='text-white text-center text-[13px] md:text-[15px]'>Design and Developed by Muhammd Mujtaba.</p>
+        <p className='text-white text-center text-[13px] md:text-[15px]'>Build with <span className='text-[#4E9FE5]'>React.js</span> & <span className='text-[#4E9FE5]'>Tailwind css</span>. Hosted on <span className='text-[#4E9FE5]'>Vercel</span></p>
     </div>
     </>
   )
--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -13,11 +13,11 @@ function Front() {
       </div>
         <div>
         <h5 className='text-[#4E9FE5] font-semibold text-[25px] md:text-[33px] h-[25px]'>Assalam O Alaikum, I'm-</h5>
-        <h3 className='text-white text-[55px] md:text-[85px] mb-1 font-black'>Muhammad Mujtaba</h3>
+        <h3 className='text-white text-[50px] md:text-[85px] mb-1 font-black'>Muhammad Mujtaba</h3>
         <ReactTyped strings={["Full Stack Web Developer","Full Stack Web Developer",]} typespeed={10} backspeed={10} loop className='text-white text-[25px] md:text-[33px] font-medium' /> 
         <p className='text-[#b8b8b8ff] text-[15px] md:text-[19px] font-medium w-[80vw] md:w-[50vw] mt-7 mb-10'>I am an aspiring web developer with a strong foundation in HTML, CSS, Tailwind, JavaScript and React.js. Recently completed a Diploma in Information Technology (2024–2025).</p>
-        <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out'>Resume</button>
-        <button className='h-[40px] w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[17px] duration-300 ease-in-out ml-4'>LinkedIn</button>
+        <button className='h-[30px] md:h-[40px] w-[110px] md:w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[14px] md:text-[17px] duration-300 ease-in-out'>Resume</button>
+        <button className='h-[30px] md:h-[40px] w-[110px] md:w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[14px] md:text-[17px] duration-300 ease-in-out ml-4'>LinkedIn</button>
         </div> 
     </div>
     </>

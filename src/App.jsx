b776ae6508a1e4a6fa33953fcec7325206f005f6
m-mujtabaaa <mmkww2007@gmail.com>
2025-11-02 07:01:41 -0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css';
 import { Typed } from 'react-typed'
 import Navbar from './components/Navbar'
 import Front from './components/Front'

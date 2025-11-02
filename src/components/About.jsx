8e71f88117777e9a2854602854c126b93fa2e1a9
m-mujtabaaa <mmkww2007@gmail.com>
2025-11-02 07:22:36 -0800

    Initial

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function About() {
   return (
     <>
-        <div className='w-[88%] pt-[25vh] px-[20px] mx-auto'>
+        <div className='w-[100%] md:w-[88%] pt-[25vh] px-[20px] mx-auto'>
           <h2 className='text-white font-bold text-[40px]'>About</h2>
-          <div className='flex h-max mb-10'>
-          <p className='text-[#b8b8b8ff] w-[50vw] pt-3 font-medium'>Hey! I'm Muhammad Mujtaba, I've been close to a computer since an early age, and been passionate about it ever since. <br/> <br/>I really liked to build stuff using no-code tools back in 2010, and from that, I explored how to code myself, fast-forward to today, I do programming in various languages and technologies, and had the privilege to worked in a Recruitment Companyand a SaaS CompanyI'm interested in building something awesome with code and automate tasks with code, currently focused on Web & Mobile Development,Open Sourceand Competitive Programming<br/><br/> When I'm not coding I play games with my friends, watch some show on Netflix, or if the weather's good, play basketball! </p>
+          <div className='static md:flex h-max mb-10'>
+          <p className='text-[#b8b8b8ff] w-full  mb-10 md:w-[50vw] pt-3 font-medium'>Hey! I'm Muhammad Mujtaba, I've been close to a computer since an early age, and been passionate about it ever since. <br/> <br/>I really liked to build stuff using no-code tools back in 2010, and from that, I explored how to code myself, fast-forward to today, I do programming in various languages and technologies, and had the privilege to worked in a Recruitment Companyand a SaaS CompanyI'm interested in building something awesome with code and automate tasks with code, currently focused on Web & Mobile Development,Open Sourceand Competitive Programming<br/><br/> When I'm not coding I play games with my friends, watch some show on Netflix, or if the weather's good, play basketball! </p>
           <div className=' w-[50vw]'>
-            <div className='border-white border-[2px] w-[350px] mx-auto h-[350px] rounded-[50%]'></div>
+            <div className='border-white border-[2px] w-[300px] mx-auto md:mx-auto h-[300px] rounded-[50%]'></div>
           </div>
         </div>
         </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Project() {
   return (
     <>
-        <div className='w-[88%] mx-auto pt-[15vh] px-[20px]'  id='Project'>
+        <div className='w-[100%] md:w-[88%] mx-auto pt-[15vh] px-[20px]'  id='Project'>
           <h2 className='text-white font-bold text-[40px] '>Project</h2>
           <p className='text-[#b8b8b8ff] w-[40vw] pt-3 font-medium'>Here's some of my projects that I have worked on.</p>
           <div className='py-[5vh]'>
-            <div className='h-[60vh] w-[45vw] rounded-xl bg-[#061120ff] border-b-[2px] border-white'>
+            <div className='h-[60vh] w-[90vw] md:w-[45vw]  rounded-xl bg-[#061120ff] border-b-[2px] border-white'>
               <div className='h-[32.5vh] bg-purple-200 rounded-t-lg'></div>
               <h2 className='text-white font-semibold py-3 px-4 text-[22px]'>TechServe Website</h2>
               <div className='bg-[#636363ff] h-[1px]'/>

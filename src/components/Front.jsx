ba81a31779932cbcf5f1ca912d47af72027bc08c
m-mujtabaaa <mmkww2007@gmail.com>
2025-11-02 09:22:59 -0800

    initial commit

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -15,7 +15,7 @@ function Front() {
         <h5 className='text-[#4E9FE5] font-semibold text-[22px] md:text-[33px] h-[25px]'>Assalam O Alaikum, I'm-</h5>
         <h3 className='text-white text-[45px] md:text-[85px] mb-7 mt-5 leading-none font-black'>Muhammad <span className='text-[67px] h-[5px] md:text-[85px]'>Mujtaba</span></h3>
         <ReactTyped strings={["Full Stack Web Developer","Full Stack Web Developer",]} typespeed={10} backspeed={10} loop className='text-white text-[22px] md:text-[33px] font-medium' /> 
-        <p className='text-[#b8b8b8ff] text-[15px] md:text-[19px] font-medium w-[90vw] md:w-[50vw] mt-7 mb-10'>I am an aspiring web developer with a strong foundation in HTML, CSS, Tailwind, JavaScript and React.js. Recently completed a Diploma in Information Technology (2024–2025).</p>
+        <p className='text-[#b8b8b8ff] text-[15px] md:text-[19px] font-medium w-[85vw] md:w-[50vw] mt-7 mb-10'>I am an aspiring web developer with a strong foundation in HTML, CSS, Tailwind, JavaScript and React.js. Recently completed a Diploma in Information Technology (2024–2025).</p>
         <button className='h-[30px] md:h-[40px] w-[110px] md:w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[14px] md:text-[17px] duration-300 ease-in-out'>Resume</button>
         <button className='h-[30px] md:h-[40px] w-[110px] md:w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[14px] md:text-[17px] duration-300 ease-in-out ml-4'>LinkedIn</button>
         </div> 

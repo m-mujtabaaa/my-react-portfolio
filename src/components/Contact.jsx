57dc87a9332c95a1e8dfbc73c8ba3629c7aad4db
m-mujtabaaa <mmkww2007@gmail.com>
2026-02-07 08:02:31 -0800

    Initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   
   return (
      <>
-    <div className='h-[85vh] w-[100%] px-[22px] bg-[#01060cff] flex justify-center items-center' id='Contact'>
+    <div className='h-[85vh] w-[100%] bg-[#01060cff] flex justify-center items-center' id='Contact'>
         <div className='h-[30vh]'>
           <h2 className='text-white font-black text-[38px] md:text-[48px] text-center mb-[5vh]'>Keep In Touch</h2>
           <p className='text-[#b8b8b8ff] text-[17px] text-center'>I'm currently specializing in <span className='text-[#4E9FE5] '>Front-end Development</span>.</p>

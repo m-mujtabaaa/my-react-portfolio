6d6a3a3e8a33cfbbd20335d4706bea965f1e8033
m-mujtabaaa <mmkww2007@gmail.com>
2026-02-15 08:28:05 -0800

    Initial commit

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -14,7 +14,7 @@ function Front() {
         <div className='pr-[0px] md:pr-[15px]'>
         <h5 className='text-[#4E9FE5] font-semibold text-[22px] md:text-[33px] h-[25px]'>Assalam O Alaikum, I'm-</h5>
         <h3 className='text-white text-[45px] md:text-[85px] mb-7 mt-5 leading-none font-black'>Muhammad <span className='text-[67px] h-[5px] md:text-[85px]'>Mujtaba</span></h3>
-        <ReactTyped strings={["Full Stack Web Developer","Full Stack Web Developer",]} typespeed={10} backspeed={10} loop className='text-white text-[22px] md:text-[33px] font-medium' /> 
+        <ReactTyped strings={["Front-End Web Developer","Front-End Web Developer",]} typeSpeed={40} backSpeed={10} loop className='text-white text-[22px] md:text-[33px] font-medium' /> 
         <p className='text-[#b8b8b8ff] text-[15px] md:text-[19px] font-medium w-full md:w-[50vw] mt-7 mb-10'>I am an aspiring web developer with a strong foundation in HTML, CSS, Tailwind, JavaScript and React.js. Recently completed a Diploma in Information Technology (2024–2025).</p>
         <button onClick={() => {window.location.href = "/MUJTABA CV.pdf";}} className='h-[30px] md:h-[40px] w-[110px] md:w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[14px] md:text-[17px] duration-300 ease-in-out'>Resume</button>
         <button onClick={() => window.open("https://www.linkedin.com/in/muhammad-mujtababa-47b763327", "_blank")} className='h-[30px] md:h-[40px] w-[110px] md:w-[125px] bg-[#0f1b2cff] hover:bg-[#1e3049ff] rounded-md text-white font-medium text-[14px] md:text-[17px] duration-300 ease-in-out ml-4'>LinkedIn</button>
